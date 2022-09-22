560266d92f82deb2e3c6dff419b2fd956e9a71c1
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-22 16:36:48 +0530

    Add project files.

---
+++ NGCQueue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGCQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue(15);
+            q.Enqueue(11);
+            q.Enqueue(12);
+            q.Enqueue(13);
+            q.Enqueue(14);
+            q.Dequeue();
+            q.Dequeue();
+            q.Peek();   
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
